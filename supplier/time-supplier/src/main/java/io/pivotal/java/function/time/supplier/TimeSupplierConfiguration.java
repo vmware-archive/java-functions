828e7959d322dc4c98250a00debc1c84e1e4bdb9
Soby Chacko <schacko@pivotal.io>
2020-01-17 17:52:43 -0500

    Adding time supplier function

---
+++ supplier/time-supplier/src/main/java/io/pivotal/java/function/time/supplier/TimeSupplierConfiguration.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.java.function.time.supplier;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.Supplier;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Soby Chacko
+ */
+@Configuration
+@EnableConfigurationProperties(TimeProperties.class)
+public class TimeSupplierConfiguration {
+
+	@Bean
+	public Supplier<String> timeSupplier(TimeProperties timeProperties) {
+		return () -> new SimpleDateFormat(timeProperties.getDateFormat()).format(new Date());
+	}
+}
