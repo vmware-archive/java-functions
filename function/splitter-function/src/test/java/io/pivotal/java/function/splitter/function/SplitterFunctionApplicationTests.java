b050c38f8c531758b04e4afafb77040bce5258a3
Artem Bilan <abilan@pivotal.io>
2020-01-28 09:47:41 -0500

    Splitter: return a List instead of Flux
    It turns out that partially reactive functions are not supported (yet) in Spring Cloud Function
    & Spring Cloud Stream
    
    * Rework a `splitter-function` to return a `List<Message<?>>` instead of `Flux<Message<?>>` -
    and now Spring Cloud Stream can understand that we would like to produce several messages
    into a target destination
    
    Related to https://github.com/spring-cloud/spring-cloud-stream/issues/1896
    
    Update mvnw symlink

--- function/splitter-function/src/test/java/io/pivotal/java/function/splitter/function/SplitterFunctionApplicationTests.java
+++ function/splitter-function/src/test/java/io/pivotal/java/function/splitter/function/SplitterFunctionApplicationTests.java
@@ -1,44 +1,45 @@
+/*
+ * Copyright (c) 2011-2020 Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.pivotal.java.function.splitter.function;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 import java.util.function.Function;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties = "splitter.expression=payload.split(',')")
 @DirtiesContext
-public abstract class SplitterFunctionApplicationTests {
+public class SplitterFunctionApplicationTests {
 
 	@Autowired
-	protected Function<Message<?>, Flux<Message<?>>> splitter;
+	Function<Message<?>, List<Message<?>>> splitter;
 
-	@TestPropertySource(properties = "splitter.expression=payload.split(',')")
-	public static class UsingExpressionIntegrationTests extends SplitterFunctionApplicationTests {
-
-		@Test
-		public void test() {
-			Flux<Message<?>> messageFlux = this.splitter.apply(new GenericMessage<>("hello,world"));
-
-			StepVerifier.create(
-					messageFlux
-							.map(Message::getPayload)
-							.cast(String.class))
-					.expectNext("hello", "world")
-					.verifyComplete();
-		}
-
+	@Test
+	public void testExpressionSplitter() {
+		List<Message<?>> messageList = this.splitter.apply(new GenericMessage<>("hello,world"));
+		assertThat(messageList).extracting(m -> m.getPayload().toString()).contains("hello", "world");
 	}
 
 }
