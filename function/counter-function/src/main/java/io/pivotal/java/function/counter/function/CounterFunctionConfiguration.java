5f3b15853d304c6d84278d050b5f2651674d338b
Christian Tzolov <christian.tzolov@gmail.com>
2020-03-24 16:31:51 +0100

    Add Counter Analytics Function
     Related to https://github.com/spring-cloud-stream-app-starters/app-starters-release/issues/222

---
+++ function/counter-function/src/main/java/io/pivotal/java/function/counter/function/CounterFunctionConfiguration.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.java.function.counter.function;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.integration.context.IntegrationContextUtils;
+import org.springframework.messaging.Message;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+/**
+ *
+ * @author Christian Tzolov
+ */
+@Configuration
+@Import(PropertiesSpelConverterConfiguration.class)
+@EnableConfigurationProperties({ CounterFunctionProperties.class })
+public class CounterFunctionConfiguration {
+
+	@Bean(name = { "counterFunction", "counterConsumer" })
+	public Function<Message<?>, Message<?>> counterFunction(CounterFunctionProperties properties, MeterRegistry[] meterRegistries,
+			@Qualifier(IntegrationContextUtils.INTEGRATION_EVALUATION_CONTEXT_BEAN_NAME) EvaluationContext context) {
+
+		return message -> {
+
+			String counterName = properties.getComputedNameExpression().getValue(context, message, CharSequence.class).toString();
+
+			// All fixed tags together are passed with every counter increment.
+			Tags fixedTags = this.toTags(properties.getTag().getFixed());
+
+			double amount = properties.getComputedAmountExpression().getValue(context, message, double.class);
+
+			Map<String, List<Tag>> allGroupedTags = new HashMap<>();
+			// Tag Expressions Counter
+			if (properties.getTag().getExpression() != null) {
+
+				Map<String, List<Tag>> groupedTags = properties.getTag().getExpression().entrySet().stream()
+						// maps a <name, expr> pair into [<name, expr#val_1>, ... <name, expr#val_N>] Tag array.
+						.map(namedExpression ->
+								toList(namedExpression.getValue().getValue(context, message)).stream()
+										.map(tagValue -> Tag.of(namedExpression.getKey(), tagValue))
+										.collect(Collectors.toList())).flatMap(List::stream)
+						.collect(Collectors.groupingBy(tag -> tag.getKey(), Collectors.toList()));
+				allGroupedTags.putAll(groupedTags);
+			}
+
+			this.count(meterRegistries, counterName, fixedTags, allGroupedTags, amount);
+
+			return message;
+		};
+	}
+
+	/**
+	 * Converts a key/value Map into Tag(key,value) list. Filters out the empty key/value pairs.
+	 * @param keyValueMap key/value map to convert into tags.
+	 * @return Returns Tags list representing every non-empty key/value pair.
+	 */
+	protected Tags toTags(Map<String, String> keyValueMap) {
+		return CollectionUtils.isEmpty(keyValueMap) ? Tags.empty() :
+				Tags.of(keyValueMap.entrySet().stream()
+						.filter(e -> StringUtils.hasText(e.getKey()) && StringUtils.hasText(e.getValue()))
+						.map(e -> Tag.of(e.getKey(), e.getValue()))
+						.collect(Collectors.toList()));
+	}
+
+	/**
+	 * Converts the input value into an list of values. If the value is not a collection/array type the result
+	 * is a single element list. For collection/array input value the result is the list of stringifie content of
+	 * this collection.
+	 * @param value input value can be array, collection or single value.
+	 * @return Returns value list.
+	 */
+	protected List<String> toList(Object value) {
+		if (value == null) {
+			return Collections.emptyList();
+		}
+
+		if ((value instanceof Collection) || ObjectUtils.isArray(value)) {
+			Collection<?> valueCollection = (value instanceof Collection) ? (Collection<?>) value
+					: Arrays.asList(ObjectUtils.toObjectArray(value));
+
+			return valueCollection.stream()
+					.filter(v -> v != null)
+					.map(Object::toString)
+					.filter(StringUtils::hasText)
+					.collect(Collectors.toList());
+		}
+		else {
+			return Arrays.asList(value.toString());
+		}
+	}
+
+	private void count(MeterRegistry[] meterRegistries, String counterName, Tags fixedTags, Map<String, List<Tag>> groupedTags, double amount) {
+		if (!CollectionUtils.isEmpty(groupedTags)) {
+			groupedTags.values().stream().map(List::size).max(Integer::compareTo).ifPresent(
+					max -> {
+						for (int i = 0; i < max; i++) {
+							Tags currentTags = Tags.of(fixedTags);
+							for (Map.Entry<String, List<Tag>> e : groupedTags.entrySet()) {
+								currentTags = (e.getValue().size() > i) ?
+										currentTags.and(e.getValue().get(i)) :
+										currentTags.and(Tags.of(e.getKey(), ""));
+							}
+
+							// Increment the counterName increment for every configured MaterRegistry.
+							for (MeterRegistry meterRegistry : meterRegistries) {
+								meterRegistry.counter(counterName, currentTags).increment(amount);
+							}
+						}
+					}
+			);
+		}
+		else {
+			// Increment the counterName increment for every configured MaterRegistry.
+			for (MeterRegistry meterRegistry : meterRegistries) {
+				meterRegistry.counter(counterName, fixedTags).increment(amount);
+			}
+		}
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	public SimpleMeterRegistry simpleMeterRegistry() {
+		return new SimpleMeterRegistry();
+	}
+}
---
+++ function/counter-function/src/main/java/io/pivotal/java/function/counter/function/PropertiesSpelConverterConfiguration.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.pivotal.java.function.counter.function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ParseException;
+import org.springframework.expression.spel.standard.SpelExpression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+/**
+ * @author Christian Tzolov
+ */
+@Configuration
+public class PropertiesSpelConverterConfiguration {
+
+	@Bean
+	@ConfigurationPropertiesBinding
+	public Converter<String, Expression> propertiesSpelConverter() {
+		return new SpelConverter();
+	}
+
+	/**
+	 * A simple converter from String to Expression.
+	 *
+	 * @author Eric Bottard
+	 */
+	public static class SpelConverter implements Converter<String, Expression> {
+
+		private SpelExpressionParser parser = new SpelExpressionParser();
+
+		@Lazy
+		@Autowired
+		private EvaluationContext evaluationContext;
+
+		@Override
+		public Expression convert(String source) {
+			try {
+				Expression expression = parser.parseExpression(source);
+				if (expression instanceof SpelExpression) {
+					((SpelExpression) expression).setEvaluationContext(evaluationContext);
+				}
+				return expression;
+			}
+			catch (ParseException e) {
+				throw new IllegalArgumentException(
+						String.format("Could not convert '%s' into a SpEL expression", source), e);
+			}
+		}
+	}
+}
